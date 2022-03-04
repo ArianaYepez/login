9f24e19a4a756e3ce087978a199b83c487eb4880
Ariana Yépez <arianaypez@gmail.com>
2022-03-04 12:01:59 -0500

    register controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,7 +7,7 @@ use App\Providers\RouteServiceProvider;
 use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
-//use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
 {
@@ -41,14 +41,43 @@ class RegisterController extends Controller
         $this->middleware('guest');
     }
 
-    /*protected function validator(array $data)
+     /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+
+
+    protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
+           /* 'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],*/
+
+            'name'=>'required|min:2|max:20|alpha',
+            'lastname'=>'required|min:2|max:40|alpha',
+            'dni'=>'required|min:2|max:20|new dniRule()',
+
+            //usando eloquent evitas inyeccion sql
+            'email'=>'required|email|unique:App\User,email',
+            //el ejemplo del profesor estaba directamente con la tabla de la bd
+           // 'email'=>'required|email|unique:users',
+
+           
+            //minimo una mayuscula
+            ////minimo un caracter especial *?etc...
+            'password'=>'required|alpha_num|min:10|same:passwordconfirm|new AlphaNumericSymbol()',
+
+          
+            'passwordconfirm'=>'required|alpha_num|min:10|new AlphaNumericSymbol()',
+            'phone'=>'nullable|numeric|min:9|max:12|new phoneRule()',
+            'country'=>'nullable',
+            'iban'=>'required|new ibanRule()',
+            'aboutme'=>'nullable|numeric|min:9|max:12',
         ]);
-    }*? 
+    }
 
     /**
      * Create a new user instance after a valid registration.
@@ -58,15 +87,25 @@ class RegisterController extends Controller
      */
   
   
-     /* protected function create(array $data)
+    protected function create(array $data)
     {
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+
+            'lastname'=> $data['lastname'],
+            'dni'=> $data['dni'],
+            'phone'=> $data['phone'],
+            'country'=> $data['country'],
+            'iban'=> $data['iban'],
+            'aboutme'=> $data ['aboutme'],
+    
         ]);
-    }*/
-    public function createUser(){
+        return redirect()->route('auth.register')->with('success', 'User created correctly');
+    }
+   //EJEMPLO DEL PROFESOR
+   /* public function createUser(){
         return view('user.createUser');
     }
 
@@ -98,17 +137,15 @@ class RegisterController extends Controller
             ////minimo un caracter especial *?etc...
             'password'=>'required|alpha_num|min:10|same:passwordconfirm|new AlphaNumericSymbol()',
 
-            //no permitir copy paste
-            'passwordconfirm'=>'required|alpha_num|min:10',
-            
           
+            'passwordconfirm'=>'required|alpha_num|min:10',
             'phone'=>'nullable|numeric|min:9|max:12|new phoneRule()',
-
             'country'=>'nullable',
             'iban'=>'required|new ibanRule()',
             'aboutme'=>'nullable|numeric|min:9|max:12',
            
         ]);
+
         $user= new User;
         $user->name= $name;
         $user->lastname= $lastname;
@@ -123,5 +160,5 @@ class RegisterController extends Controller
         $user->save();
         return redirect()->route('auth.register')->with('success', 'User created correctly');
 
-    }
+    }*/
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -74,7 +74,7 @@
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-6" oncopy="return false" onpaste="return false">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" >
 
                                 @error('password')
@@ -85,7 +85,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <div class="form-group row" oncopy="return false" onpaste="return false">
                             <label for="passwordconfirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
                             <div class="col-md-6">
