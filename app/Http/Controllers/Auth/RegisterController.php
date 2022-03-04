e117436a23348bef3aa926e2c1b6ca81bec67f13
Ariana Yépez <arianaypez@gmail.com>
2022-03-04 15:53:35 -0500

    registro sin login

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -49,7 +49,7 @@ class RegisterController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('guest');
     }
 
      /**
